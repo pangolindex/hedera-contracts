f81fa9f21935457317ed42cb5f9a863b2e25a56c
bmino <brandon@pangolin.exchange>
2023-01-17 13:56:20 -0500

    Hedera compliant Rewarder

--- contracts/staking-positions/RewarderViaMultiplierForPangoChef.sol
+++ contracts/staking-positions/RewarderViaMultiplierForPangoChef.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
+pragma solidity =0.8.15;
 
-pragma solidity 0.6.12;
-
-import "@boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol";
-import "@boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol";
+import "../hts-precompile/HederaResponseCodes.sol";
+import "../hts-precompile/HederaTokenService.sol";
 
 import "./interfaces/IRewarder.sol";
 
-contract RewarderViaMultiplierForPangoChef is IRewarder {
-    using BoringMath for uint256;
-    using BoringERC20 for IERC20;
+interface IERC20 {
+    function balanceOf(address owner) external view returns (uint256);
+}
 
-    IERC20[] public rewardTokens;
+contract RewarderViaMultiplierForPangoChef is IRewarder, HederaTokenService {
+
+    address[] public rewardTokens;
     uint256[] public rewardMultipliers;
-    address private immutable CHEF_V2;
+    address private immutable PANGO_CHEF;
     uint256 private immutable BASE_REWARD_TOKEN_DIVISOR;
 
     // @dev Ceiling on additional rewards to prevent a self-inflicted DOS via gas limitations when claim
@@ -25,41 +26,47 @@ contract RewarderViaMultiplierForPangoChef is IRewarder {
     /// @param _rewardTokens The address of each additional reward token
     /// @param _rewardMultipliers The amount of each additional reward token to be claimable for every 1 base reward (PNG) being claimed
     /// @param _baseRewardTokenDecimals The decimal precision of the base reward (PNG) being emitted
-    /// @param _chefV2 The address of the chef contract where the base reward (PNG) is being emitted
+    /// @param _pangoChef The address of the chef contract where the base reward (PNG) is being emitted
     /// @notice Each reward multiplier should have a precision matching that individual token
     constructor (
-        IERC20[] memory _rewardTokens,
+        address[] memory _rewardTokens,
         uint256[] memory _rewardMultipliers,
         uint256 _baseRewardTokenDecimals,
-        address _chefV2
-    ) public {
+        address _pangoChef
+    ) {
         require(
             _rewardTokens.length > 0
             && _rewardTokens.length <= MAX_REWARDS
             && _rewardTokens.length == _rewardMultipliers.length,
-            "RewarderViaMultiplier::Invalid input lengths"
+            "Invalid input lengths"
         );
 
         require(
             _baseRewardTokenDecimals <= 77,
-            "RewarderViaMultiplier::Invalid base reward token decimals"
+            "Invalid base reward decimals"
         );
 
         require(
-            _chefV2 != address(0),
-            "RewarderViaMultiplier::Invalid chef address"
+            _pangoChef != address(0),
+            "Invalid chef address"
         );
 
         for (uint256 i; i < _rewardTokens.length; ++i) {
-            require(address(_rewardTokens[i]) != address(0), "RewarderViaMultiplier::Cannot reward zero address");
-            require(_rewardMultipliers[i] > 0, "RewarderViaMultiplier::Invalid multiplier");
+            require(_rewardTokens[i] != address(0), "Cannot reward zero address");
+            require(_rewardMultipliers[i] > 0, "Invalid multiplier");
         }
+
+        int256 associateResponseCode = HederaTokenService.associateTokens(address(this), _rewardTokens);
+        require(associateResponseCode == HederaResponseCodes.SUCCESS, "Association failed");
 
         rewardTokens = _rewardTokens;
         rewardMultipliers = _rewardMultipliers;
         BASE_REWARD_TOKEN_DIVISOR = 10 ** _baseRewardTokenDecimals;
-        CHEF_V2 = _chefV2;
+        PANGO_CHEF = _pangoChef;
     }
+
+    // @dev Allows funding auto-rent payments
+    receive() external payable {}
 
     function onReward(
         uint256,
@@ -67,28 +74,30 @@ contract RewarderViaMultiplierForPangoChef is IRewarder {
         bool destructiveAction,
         uint256 rewardAmount,
         uint256
-    ) onlyMCV2 override external {
-        for (uint256 i; i < rewardTokens.length; ++i) {
-            uint256 pendingReward = rewardDebts[user][i].add(rewardAmount.mul(rewardMultipliers[i]) / BASE_REWARD_TOKEN_DIVISOR);
-            uint256 rewardBal = rewardTokens[i].balanceOf(address(this));
+    ) onlyChef override external {
+        uint256 tokensLength = rewardTokens.length;
+        for (uint256 i; i < tokensLength; ++i) {
+            uint256 pendingReward = rewardDebts[user][i] + (rewardAmount * rewardMultipliers[i] / BASE_REWARD_TOKEN_DIVISOR);
+            uint256 rewardBal = IERC20(rewardTokens[i]).balanceOf(address(this));
             if (!destructiveAction) {
                 rewardDebts[user][i] = pendingReward;
             } else if (pendingReward > rewardBal) {
                 rewardDebts[user][i] = pendingReward - rewardBal;
-                rewardTokens[i].safeTransfer(user, rewardBal);
+                _transferReward(rewardTokens[i], user, rewardBal);
             } else {
                 rewardDebts[user][i] = 0;
-                rewardTokens[i].safeTransfer(user, pendingReward);
+                _transferReward(rewardTokens[i], user, pendingReward);
             }
         }
     }
 
     /// @notice Shows pending tokens that can be currently claimed
-    function pendingTokens(uint256, address user, uint256 rewardAmount) external view returns (IERC20[] memory tokens, uint256[] memory amounts) {
-        amounts = new uint256[](rewardTokens.length);
-        for (uint256 i; i < rewardTokens.length; ++i) {
-            uint256 pendingReward = rewardDebts[user][i].add(rewardAmount.mul(rewardMultipliers[i]) / BASE_REWARD_TOKEN_DIVISOR);
-            uint256 rewardBal = rewardTokens[i].balanceOf(address(this));
+    function pendingTokens(uint256, address user, uint256 rewardAmount) external view returns (address[] memory tokens, uint256[] memory amounts) {
+        uint256 tokensLength = rewardTokens.length;
+        amounts = new uint256[](tokensLength);
+        for (uint256 i; i < tokensLength; ++i) {
+            uint256 pendingReward = rewardDebts[user][i] + (rewardAmount * rewardMultipliers[i] / BASE_REWARD_TOKEN_DIVISOR);
+            uint256 rewardBal = IERC20(rewardTokens[i]).balanceOf(address(this));
             if (pendingReward > rewardBal) {
                 amounts[i] = rewardBal;
             } else {
@@ -100,17 +109,18 @@ contract RewarderViaMultiplierForPangoChef is IRewarder {
 
     /// @notice Shows pending tokens including rewards accrued after the funding has been exhausted
     /// @notice these extra rewards could be claimed if more funding is added to the contract
-    function pendingTokensDebt(uint256, address user, uint256 rewardAmount) external view returns (IERC20[] memory tokens, uint256[] memory amounts) {
-        amounts = new uint256[](rewardTokens.length);
-        for (uint256 i; i < rewardTokens.length; ++i) {
-            uint256 pendingReward = rewardDebts[user][i].add(rewardAmount.mul(rewardMultipliers[i]) / BASE_REWARD_TOKEN_DIVISOR);
+    function pendingTokensDebt(uint256, address user, uint256 rewardAmount) external view returns (address[] memory tokens, uint256[] memory amounts) {
+        uint256 tokensLength = rewardTokens.length;
+        amounts = new uint256[](tokensLength);
+        for (uint256 i; i < tokensLength; ++i) {
+            uint256 pendingReward = rewardDebts[user][i] + (rewardAmount * rewardMultipliers[i] / BASE_REWARD_TOKEN_DIVISOR);
             amounts[i] = pendingReward;
         }
         return (rewardTokens, amounts);
     }
 
     /// @notice Overloaded getter for easy access to the reward tokens
-    function getRewardTokens() external view returns (IERC20[] memory) {
+    function getRewardTokens() external view returns (address[] memory) {
         return rewardTokens;
     }
 
@@ -119,11 +129,13 @@ contract RewarderViaMultiplierForPangoChef is IRewarder {
         return rewardMultipliers;
     }
 
-    modifier onlyMCV2 {
-        require(
-            msg.sender == CHEF_V2,
-            "Only MCV2 can call this function."
-        );
+    function _transferReward(address reward, address recipient, uint256 amount) private {
+        int256 transferResponseCode = HederaTokenService.transferToken(reward, address(this), recipient, int64(uint64(amount)));
+        require(transferResponseCode == HederaResponseCodes.SUCCESS, "Transfer failed");
+    }
+
+    modifier onlyChef {
+        require(msg.sender == PANGO_CHEF, "Only PangoChef");
         _;
     }
 
